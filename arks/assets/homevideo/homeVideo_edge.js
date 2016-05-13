f48d1b42de793f48f151921ee6f2e52b3f50dc1c
Bovidaeband <info@bovidae.co.uk>
2016-05-13 12:23:50 +0100

    TestARKS

---
+++ arks/assets/homevideo/homeVideo_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x3='6.0.0.400',x2='5.0.0',x5='rgba(255,255,255,1)',i='none';var g4='Arks_Website_BGvideos_2016_001_homepage_1080.mp4';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Arks_Website_BGvideos_2016_001_homepage_1080',t:'video',tag:'video',r:['0','0','1920','1080','auto','auto'],ap:'autoplay',sr:[vid+g4],pr:'auto'}],style:{'${Stage}':{isStage:true,r:['null','null','1920px','1080px','auto','auto'],overflow:'hidden',f:[x5]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-12883207");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-12883207");
